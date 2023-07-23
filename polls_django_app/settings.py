391aa591a57a418e5edf511e6386dcc54517627b
Nadia HYLARY <nhylary12@gmail.com>
2023-07-23 22:42:10 +0100

    database setup

--- polls_django_app/settings.py
+++ polls_django_app/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'polls_django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'polls_db.sqlite3',
     }
 }
 
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+1'
 
 USE_I18N = True
 
