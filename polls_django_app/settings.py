b9ca65e63dc761cd7d63e5a79bf0fb751fcba315
Nadia HYLARY <nhylary12@gmail.com>
2023-07-25 11:58:41 +0100

    postgresql db setup

--- polls_django_app/settings.py
+++ polls_django_app/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'polls_django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'polls_db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
