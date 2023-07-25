c6d721d7cb138b9404ece530b219cf38bbce8fd5
Nadia HYLARY <nhylary12@gmail.com>
2023-07-25 15:18:52 +0100

    adding views and templates

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 
 from polls.models import Question
-
 
 # Create your views here.
 
@@ -17,10 +16,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail-question.html", {"question": question})
 
 
